c482ddd705f575fd8d76e27badf6d11e447423dc
Jeet Rane <jeetrane2002rj@gmail.com>
2023-02-03 00:28:52 +0530

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,77 +1,161 @@
-import styled from "styled-components";
-import {mobile} from "../responsive";
+import React from "react";
+import './css/login.css'
+import { useState } from 'react'
+import { createUser, loginUser } from "../utils/fireBaseUtil";
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.5),
-      rgba(255, 255, 255, 0.5)
-    ),
-    url("https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
-      center;
-  background-size: cover;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+  const [phoneno, setNumber] = useState('')
+  const [address, setAddress] = useState('')
+  const [lat, setLat] = useState(null)
+  const [lng, setLng] = useState(null)
 
-const Wrapper = styled.div`
-  width: 25%;
-  padding: 20px;
-  background-color: white;
-  ${mobile({ width: "75%" })}
-`;
+  const handleCreate = () => createUser(email,password);
 
-const Title = styled.h1`
-  font-size: 24px;
-  font-weight: 300;
-`;
+  const [signClass, setSign] = useState(false);
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
+  const handleSign = () => setSign(!signClass);
 
-const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 10px 0;
-  padding: 10px;
-`;
+  
 
-const Button = styled.button`
-  width: 40%;
-  border: none;
-  padding: 15px 20px;
-  background-color: teal;
-  color: white;
-  cursor: pointer;
-  margin-bottom: 10px;
-`;
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+     console.log( "Geolocation is not supported by this browser.");
+    }
+  }
+  function showPosition(position) {
+    console.log( "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+    setLat(position.coords.latitude)
+    setLng(position.coords.longitude)
+  } 
+  
+//  async function loginUser(event){
+//   event.preventDefault();
+//   console.log("Email:"+email+" Password:"+password)
 
-const Link = styled.a`
-  margin: 5px 0px;
-  font-size: 12px;
-  text-decoration: underline;
-  cursor: pointer;
-`;
+//   signInWithEmailAndPassword(auth,email,password)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     // setCookie('email', email, { path: '/' });
 
-const Login = () => {
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
+
+  // const response = await fetch('http://192.168.34.35:4000/login', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 		},
+	// 		body: JSON.stringify({
+	// 			email,
+	// 			password,
+	// 		}),
+	// 	})
+
+	// 	const data = await response.json()
+  //   console.log(data)
+	// 	if (data.user) {
+	// 		localStorage.setItem('token', data.user)
+	// 		alert('Login successful')
+	// 		window.location.href = '/dashboard'
+	// 	} else {
+	// 		alert('Please check your username and password')
+	// 	}
+//  }
+ 
   return (
-    <Container>
-      <Wrapper>
-        <Title>SIGN IN</Title>
-        <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
-          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
-          <Link>CREATE A NEW ACCOUNT</Link>
-        </Form>
-      </Wrapper>
-    </Container>
+    <div className={signClass ? "cont s--signup" : "cont"}>
+      <div className="form sign-in">
+        <form className="sign-in-form" onSubmit={loginUser} />
+        <h2>Welcome</h2>
+        <label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email" />
+        </label>
+        <label>
+          <input value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password" />
+        </label>
+        <p className="forgot-pass">Forgot password?</p>
+        <input type="Submit" className="submit submit-signin" onClick={()=>loginUser(email,password)} />
+      </div>
+      <div className="sub-cont">
+        <div className="img">
+          <div className="img__text m--up">
+            <h3>Don't have an account? Please Sign up!</h3>
+
+          </div>
+          <div className="img__text m--in">
+            <h3>If you already has an account, just sign in.</h3>
+            <h3></h3>
+          </div>
+          <div className="img__btn" onClick={handleSign}>
+            <button className="m--up" onClick={handleSign}>Sign Up</button>
+            <button className="m--in" onClick={handleSign}>Sign In</button>
+          </div>
+        </div>
+        <div className="form sign-up">
+          <form action="/register" method="post">
+            <h2>Create your Account</h2>
+            <label>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type='text'
+                placeholder="Name" />
+            </label>
+            <label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Email" />
+            </label>
+            <label>
+              
+              <input value={phoneno}
+                onChange={(e) => setNumber(e.target.value)}
+                type="tel"
+                placeholder="Phone Number" />
+            </label>
+            <label>
+              <input value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                type="text"
+                placeholder="Address" />
+            </label>
+            <label>
+              <input value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password" />
+            </label>
+            <button type="button" className="submit" onClick={getLocation}>
+              Get Location
+            </button>
+            
+            <input type="Submit" className="submit submit-signin" onClick={()=>createUser(email,password)} />
+          </form>
+        </div>
+      </div>
+    </div>
+
+
   );
-};
+}
 
 export default Login;
